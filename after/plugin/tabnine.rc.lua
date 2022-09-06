8d07747133b9f3952c9f01e11a98e23501e71f0c
José Luis Velázquez Rodríguez <joseluisvelazquezro@gmail.com>
2022-09-06 12:28:23 -0500

    tabnine config

---
+++ after/plugin/tabnine.rc.lua
@@ -0,0 +1,10 @@
+local status, tabnine = pcall(require, 'cmp_tabnine.config')
+if (not status) then return end
+
+tabnine:setup({
+  max_lines = 1000;
+  max_num_results = 20;
+  sort = true;
+  run_on_every_keystroke = true;
+  snippet_placeholder = '..';
+})
